1c92ad060ec8c1fbc0743e57611119e54e32e797
Juan Bautista Nemi <80529710+jnemi2@users.noreply.github.com>
2022-04-20 11:26:32 -0300

    ADD - Ticket_02

--- Ticket.cpp
+++ Ticket.cpp
@@ -9,6 +9,13 @@ Ticket::Ticket(){
 
 Ticket::Ticket(string datos){
     this->datos = datos;
+    this->fecha.AsignarAAAA(2022);
+    this->fecha.AsignarMM(4);
+    this->fecha.AsignarDD(20);
+}
+
+Ticket::~Ticket(){
+
 }
 
 
@@ -17,5 +24,6 @@ void Ticket::mostrarDatos(){
 }
 
 void Ticket::parsearDatos(){
-    cout<<"Datos parseados: "<<this->datos<<endl;
+    Fecha f = this->fecha;
+    cout<<"(" + to_string(f.ObtenerDD()) + "/" + to_string(f.ObtenerMM()) + "/" + to_string(f.ObtenerAAAA()) + ") " + this->datos<<endl;
 }
--- main.cpp
+++ main.cpp
@@ -7,6 +7,8 @@ using namespace std;
 int main()
 {
     cout<<"Hello, world!"<<endl;
+    Ticket unTicket("1234567890");
+    unTicket.parsearDatos();
 
     return 0;
 }
